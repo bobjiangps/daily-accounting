17b5906cecd17875b9c0142d1872bcc20fee3462
bobjiangps <jbsv43@sina.com>
2020-12-30 18:07:20 +0800

    add accounting models

--- accounting/models.py
+++ accounting/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Currency(models.Model):
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Account(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    currency = models.ForeignKey(Currency, on_delete=models.SET_NULL, default=1)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100)
+    parent = models.ForeignKey(Category, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+
+class Record(models.Model):
+    time_of_occurrence = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.SET_NULL, null=True, blank=True)
+    currency = models.ForeignKey(Currency, on_delete=models.SET_NULL, default=1)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    comment = models.CharField(max_length=500)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(default=timezone.now)
