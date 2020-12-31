32e2853762918e53ce9aa5eb460615b4770e6325
bobjiangps <jbsv43@sina.com>
2020-12-31 14:18:47 +0800

    update model field and register in admin

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Currency)
+admin.site.register(Account)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(HistoryRecord)
--- accounting/models.py
+++ accounting/models.py
@@ -13,7 +13,7 @@ class Currency(models.Model):
 class Account(models.Model):
     name = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
-    currency = models.ForeignKey(Currency, on_delete=models.SET_NULL, default=1)
+    currency = models.ForeignKey(Currency, on_delete=models.SET_NULL, null=True, default=1)
     created_date = models.DateTimeField(default=timezone.now)
     updated_date = models.DateTimeField(default=timezone.now)
 
@@ -32,17 +32,17 @@ class Category(models.Model):
 class SubCategory(models.Model):
     name = models.CharField(max_length=100)
     icon = models.CharField(max_length=100)
-    parent = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    parent = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
 
 
-class Record(models.Model):
+class HistoryRecord(models.Model):
     time_of_occurrence = models.DateTimeField(default=timezone.now)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     sub_category = models.ForeignKey(SubCategory, on_delete=models.SET_NULL, null=True, blank=True)
-    currency = models.ForeignKey(Currency, on_delete=models.SET_NULL, default=1)
+    currency = models.ForeignKey(Currency, on_delete=models.SET_NULL, null=True, default=1)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     comment = models.CharField(max_length=500)
     created_date = models.DateTimeField(default=timezone.now)
