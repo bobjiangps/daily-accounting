a33830f08619d4e1b33aaba161c22f8c606e255d
bobjiangps <jbsv43@sina.com>
2021-01-04 15:13:34 +0800

    render model data to front

--- accounting/views.py
+++ accounting/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 
 def index(request):
-    return render(request, 'accounting/index.html')
+    all_accounts = Account.objects.all()
+    sub_categories = SubCategory.objects.all()
+    context = {
+        'accounts': all_accounts,
+        'sub_categories': sub_categories
+               }
+    return render(request, 'accounting/index.html', context)
