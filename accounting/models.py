0c7718e8c60f50310e6c306d474c62a08e85fa18
bobjiangps <jbsv43@sina.com>
2021-01-04 18:20:09 +0800

    update category model

--- accounting/models.py
+++ accounting/models.py
@@ -14,6 +14,7 @@ class Account(models.Model):
     name = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     currency = models.ForeignKey(Currency, on_delete=models.SET_NULL, null=True, default=1)
+    icon = models.CharField(max_length=100, null=True)
     created_date = models.DateTimeField(default=timezone.now)
     updated_date = models.DateTimeField(default=timezone.now)
 
@@ -22,8 +23,13 @@ class Account(models.Model):
 
 
 class Category(models.Model):
+    CATEGORY_TYPES = (
+       ("Income", "income"),
+       ("Expense", "expense")
+    )
     name = models.CharField(max_length=100)
     icon = models.CharField(max_length=100)
+    category_type = models.CharField(choices=CATEGORY_TYPES, default=CATEGORY_TYPES[0][1], max_length=100)
 
     def __str__(self):
         return self.name
