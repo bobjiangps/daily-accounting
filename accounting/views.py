14b4f96016b8f40b4bb0c4cd981762c367a1122e
bobjiangps <jbsv43@sina.com>
2021-01-08 18:23:14 +0800

    update index page and add debug code for ajax

--- accounting/views.py
+++ accounting/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 from .models import *
 
 
 def index(request):
     all_accounts = Account.objects.all()
-    categories = Category.objects.all()
-    sub_categories = SubCategory.objects.all()
+    # categories = Category.objects.all()
+    # sub_categories = SubCategory.objects.all()
     currencies = Currency.objects.all()
-    ie_types = []
-    for t in Category.CATEGORY_TYPES:
-        ie_types.append(t[0])
+    ie_types = Category.CATEGORY_TYPES
     context = {
         'accounts': all_accounts,
-        'categories': categories,
-        'sub_categories': sub_categories,
+        # 'categories': categories,
+        # 'sub_categories': sub_categories,
         'currencies': currencies,
         'ie_types': ie_types
                }
     return render(request, 'accounting/index.html', context)
+
+
+def retrieve_category(request):
+    ie_type = request.POST.get('ie_type')
+    categories = Category.objects.filter(category_type=ie_type)
+    # return HttpResponse(f'{"categories": {categories}}', content_type='application/json')
+    return JsonResponse(f'{"categories": {categories}}')
